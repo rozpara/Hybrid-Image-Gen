173a9dbdb80271a6233982023ce3aa663f2df856
Roz <rokpara@gmail.com>
2015-10-05 02:24:52 -0400

    Created rough script for main functionality

--- hybrid.py
+++ hybrid.py
@@ -1,3 +1,16 @@
-import numpy as np
 import cv2
 
+
+high  = cv2.imread('einstein.png')
+low = cv2.imread('marilyn.png')
+
+m,n,d = low.shape
+
+blur = cv2.GaussianBlur(low, (49,49), 0)
+
+blur2 = cv2.GaussianBlur(high, (7,7), 0)
+sharp = high - blur2
+
+cv2.imshow('img', sharp + blur)
+
+cv2.waitKey(0)
