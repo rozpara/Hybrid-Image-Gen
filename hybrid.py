4758769e3b861b713e2254538b514a409934852a
Roseline <rokpara@gmail.com>
2015-10-05 20:49:42 -0400

    Produces a nice hybrid image with given frequencies

--- hybrid.py
+++ hybrid.py
@@ -1,16 +1,20 @@
 import cv2
 
+#import images
+high = cv2.imread('paul.jpg',0)
+low = cv2.imread('zach.jpg',0)
 
-high  = cv2.imread('einstein.png')
-low = cv2.imread('marilyn.png')
+m,n = low.shape
 
-m,n,d = low.shape
+#apply the highpass filter
+blur = cv2.GaussianBlur(low, (111,111), 0)
 
-blur = cv2.GaussianBlur(low, (49,49), 0)
-
-blur2 = cv2.GaussianBlur(high, (7,7), 0)
+#apply highpass gaussian filter and subtract it from the original image
+blur2 = cv2.GaussianBlur(high, (35,35),0)
 sharp = high - blur2
 
+#show the hybrid image
 cv2.imshow('img', sharp + blur)
 
+#press anykey
 cv2.waitKey(0)
